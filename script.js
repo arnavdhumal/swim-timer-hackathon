60045ea25037589ee025ba4ea21a3f83178e37e8
arnavdhumal <arnavdhumal>
2021-12-12 10:05:32 -0800

    buttons

--- script.js
+++ script.js
@@ -2,5 +2,14 @@ function select_sport() {
     var sportvalue = document.getElementById("sport").value;
     if (sportvalue == "Swimming") {
         document.getElementById("swim_form").style = 'block';
+        function start() {
+            console.log("start")
+        }
+        function lap() {
+            console.log("lap")
+        }
+        function end() {
+            console.log("end")
+        }
     }
 }
