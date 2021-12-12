a0112a4a401e86429cd14dd15adbb80c534d8355
arnavdhumal <arnavdhumal>
2021-12-12 14:43:34 -0800

    buttons

--- script.js
+++ script.js
@@ -2,14 +2,23 @@ function select_sport() {
     var sportvalue = document.getElementById("sport").value;
     if (sportvalue == "Swimming") {
         document.getElementById("swim_form").style = 'block';
-        function start() {
-            console.log("start")
-        }
-        function lap() {
-            console.log("lap")
-        }
-        function end() {
-            console.log("end")
-        }
     }
 }
+function start() {
+    console.log("start")
+};
+function lap() {
+    console.log("lap")
+};
+function stop() {
+    console.log("stop")
+};
+/*document.getElementById("start").addEventListener("click", function() {
+    console.log("start")
+});
+document.getElementById("lap").addEventListener("click", function() {
+    console.log("lap")
+});
+document.getElementById("end").addEventListener("click", function() {
+    console.log("start")
+});*/
