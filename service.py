ec229f40b651543a84f57c00fcc645b0d8b62c0f
Ishaan <ishaan.florian@gmail.com>
2021-12-12 15:43:01 -0800

    sql

--- service.py
+++ service.py
@@ -2,7 +2,7 @@ def user_details():
   name = input("Please enter your name ")       
   return name
 
-name = user_details()
+
 
 def getsport():
   options = ["swimming", "running"]  
@@ -11,6 +11,19 @@ def getsport():
   sel_sport = input("select a sport: ") 
   return options[int(sel_sport)]
 
-sport = getsport()
 
-print ("hi " + name + " you are planning to  play " + sport )
+def get_event_details():
+  pool_length = input("Please enter the pool length in yards ")
+  stroke = input("What stroke are you swimming ")
+  event = {
+    "pool_length": pool_length,
+    "stroke": stroke
+  }   
+  return event
+
+name = user_details()
+sport = getsport()
+event = get_event_details()
+
+
+print ("Hi " + name + " you are planning to  play " + sport +".")
