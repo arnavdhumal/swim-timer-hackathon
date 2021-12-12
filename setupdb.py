bbf515abbefd9b9e08163856e864e46b7aff3d27
Ishaan <ishaan.florian@gmail.com>
2021-12-12 15:41:34 -0800

    sql

---
+++ setupdb.py
@@ -0,0 +1,3 @@
+import sqlite3
+conn = sqlite3.connect('sports-timer.db')
+# conn.execute("CREATE TABLE planes (id INTEGER PRIMARY KEY, manufacturer TEXT, model TEXT, airline TEXT, quantity INTEGER)")
