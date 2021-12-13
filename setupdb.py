51e70d69b319491adbfba1dd3383af3a8ac91112
Ishaan <ishaan.florian@gmail.com>
2021-12-12 17:54:47 -0800

    test

--- setupdb.py
+++ setupdb.py
@@ -1,6 +1,7 @@
 import sqlite3
-conn = sqlite3.connect('sports-timer.db')
-# conn.execute("CREATE TABLE planes (id INTEGER PRIMARY KEY, manufacturer TEXT, model TEXT, airline TEXT, quantity INTEGER)")
+conn = sqlite3.connect('sportstimer.db')
+conn.execute("CREATE TABLE sportstimer (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, sport_type TEXT, sport TEXT, length INTEGER, total_time REAL)")
 
 
 
+                                      
